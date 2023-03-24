49740f080fdb94f353ed645dcd4d30c706450b88
myghcs1 <120685008+myghcs1@users.noreply.github.com>
2023-03-24 19:12:47 +0330

    env

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import time, threading, requests
+import time, threading, requests, os
 from flask import Flask
 
 app = Flask(__name__)
 t = time.time()
-
+print(os.environ)
 
 def keepAlive():
     while 1:
