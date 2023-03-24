9896f2509e99a08df22c66fa5d48bc893bb03f2e
myghcs1 <120685008+myghcs1@users.noreply.github.com>
2023-03-24 15:53:21 +0330

    keep

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-import time
+import time, threading
 from flask import Flask
 
 app = Flask(__name__)
 t = time.time()
+
+
+def keepAlive():
+    while 1:
+        y = requests.get('https://b234561.onrender.com/')
+        time.sleep(666)
+
+
+threading.Thread(target=keepAlive).start()
 
 
 @app.route('/')
